cbe296bd00e295ae76802d1d8089588fc946d562
joseortiz <joseortiz87@gmail.com>
2018-07-03 09:38:44 -0700

    commit

---
+++ JavaCertification/src/com/altimetrik/Altimetrik.java
@@ -0,0 +1,12 @@
+package com.altimetrik;
+
+import java.util.Scanner;
+
+public class Altimetrik {
+	private static final Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String args[]){
+		
+		System.out.println();
+	}
+}
